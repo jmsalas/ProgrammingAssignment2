f9ab0b5e4db3d4f604ad288898198aeda6989c79
Juan Salas <jmsalas@gmail.com>
2015-05-21 23:16:31 -0300

    Fixed small indentation error.

--- cachematrix.R
+++ cachematrix.R
@@ -40,8 +40,8 @@ cacheSolve <- function(x, ...) {
         inverse <- x$getinverse()
         
         if(!is.null(inverse)) {
-            message("getting cached data")
-            return(inverse)
+                message("getting cached data")
+                return(inverse)
         }
         
         data <- x$get()
